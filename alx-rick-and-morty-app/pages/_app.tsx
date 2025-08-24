c7e846347d0b800cac698cbf61b5302111b8db30
Oluwaseyi <seyistry@gmail.com>
2025-08-24 15:29:24 +0100

    first commit

---
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -0,0 +1,14 @@
+import ErrorBoundary from '@/components/ErrorBoundary';
+import type { AppProps } from "next/app";
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
